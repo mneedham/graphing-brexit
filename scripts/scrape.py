136e42f7a69786c3863bfb6df7cbfaeaad325174
Mark Needham <m.h.needham@gmail.com>
2019-03-30 14:08:48 +0000

    Adding new vote files

---
+++ scripts/scrape.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+from neo4j import GraphDatabase
+
+driver = GraphDatabase.driver("bolt://localhost", auth=("neo4j", "neo"))
+
+with open("raw_data/indicate-vote.html", "r") as file, driver.session() as session:
+    soup = BeautifulSoup(file.read(), 'html.parser')
+    motions = [m.text.strip() for m in soup.find_all("div", "key-list")[1:]]
+
+    for index, motion in enumerate(motions):
+        session.run("""
+        MERGE (m:Motion {id: $id})
+        SET m.name = $name
+        """, {"id": index+1, "name": motion})
+
+        for one_mp in  soup.find_all("div", class_="int-row--mp"):
+            name = one_mp.find("div", "int-cell--name").text.strip()
+            party = one_mp.find("div", "int-cell--party").text.strip()
+
+            session.run("""
+            MERGE (person:Person {name: $name})
+            MERGE (party:Party {name: $party})
+            MERGE (person)-[:MEMBER_OF]->(party)
+            """, {"name": name, "party": party})
+
+            votes = [item.get("class")
+                     for item in one_mp.find_all("div", class_="gv-vote-blob")]
+            for index, vote in enumerate(votes):
+                v = [item for item in vote if item != "gv-vote-blob"][0].replace("gv-", "")
+                print(index, v)
+
+                if v == "for":
+                    session.run("""
+                    MATCH (person:Person {name: $name})
+                    MERGE (motion:Motion {id: $id})
+                    MERGE (person)-[:FOR]->(motion)
+                    """, {"name": name, "id": index+1})
+
+                if v == "against":
+                    session.run("""
+                    MATCH (person:Person {name: $name})
+                    MERGE (motion:Motion {id: $id})
+                    MERGE (person)-[:AGAINST]->(motion)
+                    """, {"name": name, "id": index+1})
+
+                if v == "did-not-vote":
+                    session.run("""
+                    MATCH (person:Person {name: $name})
+                    MERGE (motion:Motion {id: $id})
+                    MERGE (person)-[:DID_NOT_VOTE]->(motion)
+                    """, {"name": name, "id": index+1})
--- scripts/scrape_to_csv.py
+++ scripts/scrape_to_csv.py
@@ -10,11 +10,11 @@ map = {
     "did-not-vote": "DID_NOT_VOTE"
 }
 
-with open("indicate-vote.html", "r") as file, \
-     open("data/motions.csv", "w") as motions_file, \
-     open("data/mps.csv", "w") as mps_file, \
-     open("data/votes.csv", "w") as votes_file, \
-     driver.session() as session:
+with open("raw_data/indicate-vote.html", "r") as file, \
+        open("data/motions.csv", "w") as motions_file, \
+        open("data/mps.csv", "w") as mps_file, \
+        open("data/votes.csv", "w") as votes_file, \
+        driver.session() as session:
     motions_writer = csv.writer(motions_file, delimiter=",")
     mps_writer = csv.writer(mps_file, delimiter=",")
     votes_writer = csv.writer(votes_file, delimiter=",")
@@ -27,10 +27,9 @@ with open("indicate-vote.html", "r") as file, \
     motions = [m.text.strip() for m in soup.find_all("div", "key-list")[1:]]
 
     for index, motion in enumerate(motions):
-        motions_writer.writerow([index+1, motion])
+        motions_writer.writerow([index + 1, motion])
 
-
-    for one_mp in  soup.find_all("div", class_="int-row--mp"):
+    for one_mp in soup.find_all("div", class_="int-row--mp"):
         name = one_mp.find("div", "int-cell--name").text.strip()
         party = one_mp.find("div", "int-cell--party").text.strip()
 
@@ -41,4 +40,4 @@ with open("indicate-vote.html", "r") as file, \
             v = [item for item in vote if item != "gv-vote-blob"][0].replace("gv-", "")
             print(index, v)
 
-            votes_writer.writerow([name, index+1, map[v]])
+            votes_writer.writerow([name, index + 1, map[v]])
